9b3541f2b105a23adf591c8fdb8cdabaf26bbfc4
andrey <enasd@asd>
2022-07-17 04:01:46 +0700

    change attack and damage bars

--- ui.py
+++ ui.py
@@ -21,6 +21,16 @@ class ProgressBarType(Enum):
     ATTACK_BASE = auto()
 
 
+class Visible(Enum):
+    VISIBLE = auto()
+    INVISIBLE = auto()
+
+
+class Orientation(Enum):
+    VERTICAL = auto()
+    HORIZONTAL = auto()
+
+
 DAMAGE_PRINT_LIMIT = 1000
 
 
@@ -30,7 +40,7 @@ def convert_long_int(value: int) -> str:
     return str(value)
 
 
-def create_label(value: str = '', color: str = 'white'):
+def create_label(value: str = '', color: str = 'white') -> QLabel:
     label = QLabel(value)
     label.setFont(QFont('Monospace', 10))
     label.setAlignment(Qt.AlignLeft | Qt.AlignTop)
@@ -38,10 +48,17 @@ def create_label(value: str = '', color: str = 'white'):
     return label
 
 
+def create_form() -> QFormLayout:
+    form = QFormLayout()
+    form.setHorizontalSpacing(0)
+    form.setVerticalSpacing(0)
+    form.setRowWrapPolicy(QFormLayout.DontWrapRows)
+    return form
+
+
 class Param:
     def __init__(self, title: str, color: str):
-        self.form = QFormLayout()
-        self.form.setHorizontalSpacing(0)
+        self.form = create_form()
         self.title = create_label(title, color)
         self.value = create_label(color=color)
         self.form.addRow(self.title, self.value)
@@ -81,7 +98,7 @@ class CharacterStat:
             convert_long_int(damage), convert_long_int(last_damage), convert_long_int(damage_absorb)))
 
     def set_special_attack(self, name: str, ab: int, dc: int, result: str) -> None:
-        self.special_attack.value.setText('{:>2} {:>2}({:>2}/{:>4})'.format(name, ab, dc, result[:4]))
+        self.special_attack.value.setText('{:>2} {:>2}({:>2}/{:>3})'.format(name, ab, dc, result[:4]))
 
     def set_experience(self, experience: int) -> None:
         self.experience.value.setText('{:>3}'.format(convert_long_int(experience)))
@@ -89,74 +106,87 @@ class CharacterStat:
 
 class UserInterface:
     def __init__(self, widget: QWidget):
-        self.form = QFormLayout()
-        self.form.setHorizontalSpacing(0)
-        self.form.setVerticalSpacing(0)
-        self.form.setRowWrapPolicy(QFormLayout.DontWrapRows)
-        widget.setLayout(self.form)
-
         self.progress_bar_dict: typing.Dict[ProgressBarType, QProgressBar] = {}
 
-        self.progress_bar_dict.update({
-            ProgressBarType.TARGET_HP: self.create_progress_bar(
-                '%v/0 TARGET RD', 0, 0, 1,
-                UserInterface._get_style('#ffff910c'),
-                True),
-        })
+        self.main_form = create_form()
+        widget.setLayout(self.main_form)
 
-        self.target_stat = CharacterStat()
-        self.form.addRow(self.target_stat.h_box)
-
-        self.progress_bar_dict.update({
-            ProgressBarType.PLAYER_HP: self.create_progress_bar(
-                '%v/0 RD', 0, 0, 1,
-                UserInterface._get_style('#ffff0000'),
-                True),
-        })
+        self.main_form.addRow(self.create_progress_bar(
+            ProgressBarType.PLAYER_HP,
+            '%v/0 RD', 0, 0, 1,
+            UserInterface._get_style('#ffff0000'),
+            Visible.VISIBLE,
+        ))
 
         self.player_stat = CharacterStat()
-        self.form.addRow(self.player_stat.h_box)
+        self.main_form.addRow(self.player_stat.h_box)
 
-        self.progress_bar_dict.update({
-            ProgressBarType.DAMAGE_PER_ROUND: self.create_progress_bar(
-                '%v Damage per round', 0, 0, 0,
-                UserInterface._get_style('#99ff7b06'),
-                False),
-            ProgressBarType.KNOCKDOWN: self.create_progress_bar(
-                '%v ms Knockdown', 0, 0, KNOCKDOWN_PVE_CD,
-                UserInterface._get_style('#99bd00ff'),
-                False),
-            ProgressBarType.KNOCKDOWN_MISS: self.create_progress_bar(
-                '%v ms Knockdown', 0, 0, KNOCKDOWN_PVE_CD,
-                UserInterface._get_style('#99bd00ff', additional_chunk='width: 10px; margin: 0.5px;'),
-                False),
-            ProgressBarType.STUNNING_FIST: self.create_progress_bar(
-                '%v ms Stunning fist', 0, 0, STUNNING_FIST_DURATION,
-                UserInterface._get_style('#99ffffff'),
-                False),
-            ProgressBarType.STEALTH_MODE_CD: self.create_progress_bar(
-                '%v ms Stealth mode cooldown', 0, 0, STEALTH_MODE_CD,
-                UserInterface._get_style('#ff3472ff'),
-                False),
-            ProgressBarType.ATTACK_BASE: self.create_progress_bar(
-                '%v Attack base', 0, 0, 0,
-                UserInterface._get_style('#9917b402'),
-                False),
-        })
+        self.main_form.addRow(self.create_progress_bar(
+            ProgressBarType.TARGET_HP,
+            '%v/0 TARGET RD', 0, 0, 1,
+            UserInterface._get_style('#ffff910c'),
+            Visible.VISIBLE,
+        ))
+
+        self.target_stat = CharacterStat()
+        self.main_form.addRow(self.target_stat.h_box)
+
+        self.main_form.addRow(self.create_progress_bar(
+            ProgressBarType.KNOCKDOWN,
+            '%v ms Knockdown', 0, 0, KNOCKDOWN_PVE_CD,
+            UserInterface._get_style('#99bd00ff'),
+            Visible.INVISIBLE,
+        ))
+        self.main_form.addRow(self.create_progress_bar(
+            ProgressBarType.KNOCKDOWN_MISS,
+            '%v ms Knockdown', 0, 0, KNOCKDOWN_PVE_CD,
+            UserInterface._get_style('#99bd00ff', additional_chunk='width: 10px; margin: 0.5px;'),
+            Visible.INVISIBLE,
+        ))
+        self.main_form.addRow(self.create_progress_bar(
+            ProgressBarType.STUNNING_FIST,
+            '%v ms Stunning fist', 0, 0, STUNNING_FIST_DURATION,
+            UserInterface._get_style('#99ffffff'),
+            Visible.INVISIBLE,
+        ))
+        self.main_form.addRow(self.create_progress_bar(
+            ProgressBarType.STEALTH_MODE_CD,
+            '%v ms Stealth mode cooldown', 0, 0, STEALTH_MODE_CD,
+            UserInterface._get_style('#ff3472ff'),
+            Visible.INVISIBLE,
+        ))
+
+        self.attack_damage_bar = QHBoxLayout()
+        self.main_form.addRow(self.attack_damage_bar)
+
+        self.attack_damage_bar.addWidget(self.create_progress_bar(
+            ProgressBarType.ATTACK_BASE,
+            '%v Attack base', 0, 0, 0,
+            UserInterface._get_style('#9917b402'),
+            Visible.INVISIBLE,
+        ))
+
+        self.attack_damage_bar.addWidget(self.create_progress_bar(
+            ProgressBarType.DAMAGE_PER_ROUND,
+            '%v Damage per round', 0, 0, 0,
+            UserInterface._get_style('#99ff7b06'),
+            Visible.INVISIBLE,
+            inverted=True,
+        ))
 
         self.low_hp_label = QLabel("LOW HP")
         self.low_hp_label.setFont(QFont('Monospace', 32))
         self.low_hp_label.setAlignment(Qt.AlignCenter)
         self.low_hp_label.setStyleSheet('background-color: rgba(0,0,0,0%); color: red')
         self.low_hp_label.setVisible(False)
-        self.form.addRow(self.low_hp_label)
+        self.main_form.addRow(self.low_hp_label)
 
         self.main_label = QLabel("")
         self.main_label.setFont(QFont('Monospace', 10))
         self.main_label.setAlignment(Qt.AlignLeft | Qt.AlignTop)
         self.main_label.setStyleSheet('background-color: rgba(0,0,0,0%); color: white')
         self.main_label.setVisible(False)
-        self.form.addRow(self.main_label)
+        self.main_form.addRow(self.main_label)
 
     def set_main_label_text(self, text: str, visible: bool) -> None:
         self.main_label.setVisible(visible)
@@ -183,12 +213,14 @@ class UserInterface:
 
     def create_progress_bar(
             self,
+            type: ProgressBarType,
             title_format: str,
             cur_value: int,
             min_value: int,
             max_value: int,
             style: str,
-            visible: bool,
+            visible: Visible,
+            inverted: bool = False,
     ) -> QProgressBar:
         pb = QProgressBar()
         pb.setFormat(title_format)
@@ -197,8 +229,10 @@ class UserInterface:
         pb.setMaximum(max_value)
         pb.setFont(QFont('Monospace', 10))
         pb.setStyleSheet(style)
-        pb.setVisible(visible)
-        self.form.addRow(pb)
+        pb.setVisible(visible == Visible.VISIBLE)
+        pb.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+        pb.setInvertedAppearance(inverted)
+        self.progress_bar_dict[type] = pb
         return pb
 
     def upgrade_player_hp_progress_bar(
@@ -232,7 +266,7 @@ class UserInterface:
             cur_value: int = 0,
             min_value: typing.Optional[int] = None,
             max_value: typing.Optional[int] = None,
-            visible: bool = True,
+            visible: Visible = Visible.VISIBLE,
     ) -> None:
         pb = self.progress_bar_dict[bar_type]
         pb.setValue(cur_value)
@@ -240,4 +274,4 @@ class UserInterface:
             pb.setMinimum(min_value)
         if max_value is not None:
             pb.setMaximum(max_value)
-        pb.setVisible(visible)
+        pb.setVisible(visible == Visible.VISIBLE)
